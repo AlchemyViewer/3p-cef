b4ef30d15293f86087d7622017e79d9e44d488b0
Rye Mutt <rye@alchemyviewer.org>
2020-10-12 18:26:47 -0400

    Update CEF to 4240 0b3aeae

--- build-cmd.sh
+++ build-cmd.sh
@@ -33,7 +33,7 @@ cef_stage_dir="${stage}/cef"
 # https://bitbucket.org/chromiumembedded/cef/wiki/BranchesAndBuilding.md#markdown-header-release-branches
 # as can this one: https://www.chromium.org/developers/calendar
 # E.G. Branch 4044 represents Chromium/CEF 81.x
-cef_branch_number=4183
+cef_branch_number=4240
 
 # The commit hash in the branch we want to
 # check out from. One way to determine the hash to use is to look at the commits
@@ -41,7 +41,7 @@ cef_branch_number=4183
 # https://bitbucket.org/chromiumembedded/cef/commits/branch/3987 and pick the
 # commit hash the looks sensible - often something like "bumped CEF/Chromium
 # to version x.xx.xx"
-cef_commit_hash=b045a6e
+cef_commit_hash=0b3aeae
 
 # Turn on the proprietary codec support (the main reason for building from source vs using
 # the Spotify open source builds here http://opensource.spotify.com/cefbuilds/index.html)
