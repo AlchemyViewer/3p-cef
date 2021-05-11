9a44f9dd1851af0786506cb89d0f509be6c29cec
Rye Mutt <rye@alchemyviewer.org>
2021-05-11 03:28:07 -0400

    Update to CEF 4430 7a604aa

--- tools/build.bat
+++ tools/build.bat
@@ -28,8 +28,8 @@
 @set DEFAULT_ROOT_CODE_DIRECTORY=\code
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
-@set DEFAULT_BRANCH=4389
-@set DEFAULT_CEF_COMMIT_HASH="b36241d"
+@set DEFAULT_BRANCH=4430
+@set DEFAULT_CEF_COMMIT_HASH="7a604aa"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
