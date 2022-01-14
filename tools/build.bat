5c1b491d0e0c15d77b2232d837c4ed3eb7848588
Rye Mutt <rye@alchemyviewer.org>
2022-01-14 18:25:48 -0500

    Update CEF to 4692 2b00258

--- tools/build.bat
+++ tools/build.bat
@@ -28,8 +28,8 @@
 @set DEFAULT_ROOT_CODE_DIRECTORY=\code
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
-@set DEFAULT_BRANCH=4664
-@set DEFAULT_CEF_COMMIT_HASH="20e2617"
+@set DEFAULT_BRANCH=4692
+@set DEFAULT_CEF_COMMIT_HASH="2b00258"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
