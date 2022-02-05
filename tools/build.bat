113c1aa4d302671a9f2a6583b5da65992cb3530f
Rye Mutt <rye@alchemyviewer.org>
2022-02-05 18:53:00 -0500

    Bump to cef 4758 358ca26

--- tools/build.bat
+++ tools/build.bat
@@ -29,7 +29,7 @@
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
 @set DEFAULT_BRANCH=4758
-@set DEFAULT_CEF_COMMIT_HASH="42d1b7e"
+@set DEFAULT_CEF_COMMIT_HASH="358ca26"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
