14306d985c05ba935532d7bfc26f690a35920217
Rye Mutt <rye@alchemyviewer.org>
2022-02-08 17:11:12 -0500

    Update to cef 4758 63805e6

--- build-cmd.sh
+++ build-cmd.sh
@@ -41,7 +41,7 @@ cef_branch_number=4758
 # https://bitbucket.org/chromiumembedded/cef/commits/branch/4389 and pick the
 # commit hash the looks sensible - often something like "bumped CEF/Chromium
 # to version x.xx.xx"
-cef_commit_hash=358ca26
+cef_commit_hash=63805e6
 
 # Turn on the proprietary codec support (the main reason for building from source vs using
 # the Spotify open source builds here http://opensource.spotify.com/cefbuilds/index.html)
