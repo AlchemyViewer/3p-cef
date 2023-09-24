73d5de2938ea84db3b7e0155cd80746a217b3037
Rye Mutt <rye@alchemyviewer.org>
2023-09-23 23:46:52 -0400

    CEF 5938 a26f38b

--- tools/build.bat
+++ tools/build.bat
@@ -28,8 +28,8 @@
 @set DEFAULT_ROOT_CODE_DIRECTORY=\code
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
-@set DEFAULT_BRANCH=5845
-@set DEFAULT_CEF_COMMIT_HASH="5332865"
+@set DEFAULT_BRANCH=5938
+@set DEFAULT_CEF_COMMIT_HASH="a26f38b"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
