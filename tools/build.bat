b4e584a7425e9a474129251cf9e5e0286c559e27
Rye Mutt <rye@alchemyviewer.org>
2023-12-09 20:51:30 -0500

    Update CEF to 6099 120.0.6099.62

--- tools/build.bat
+++ tools/build.bat
@@ -28,8 +28,8 @@
 @set DEFAULT_ROOT_CODE_DIRECTORY=\code
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
-@set DEFAULT_BRANCH=6045
-@set DEFAULT_CEF_COMMIT_HASH="f768881"
+@set DEFAULT_BRANCH=6099
+@set DEFAULT_CEF_COMMIT_HASH="f4af2dd"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
